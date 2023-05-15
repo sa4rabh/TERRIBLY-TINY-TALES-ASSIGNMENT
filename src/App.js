a009cdbfc121c5a27886f0eb3d1c12fefee74530
saurabh kumar <86232271+sa4rabh@users.noreply.github.com>
2023-05-15 08:00:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Histogram from './Histogram';
 
-const WordFrequencyAnalyzer = () => {
+const App = () => {
   const [histogramData, setHistogramData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -15,17 +15,17 @@ const WordFrequencyAnalyzer = () => {
       const topWords = sortedWordCount.slice(0, 20);
       setHistogramData(topWords);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error in fetching data from the link:', error);
     } finally {
       setIsLoading(false);
     }
   };
 
   const countWords = (text) => {
-    // Split the text into an array of words
+    // Spliting the text into an array of words
     const words = text.toLowerCase().split(/\W+/);
 
-    // Count the occurrence of each word
+    // Counting the occurrence of each word  
     const wordCount = {};
     for (let word of words) {
       if (wordCount[word]) {
@@ -39,25 +39,25 @@ const WordFrequencyAnalyzer = () => {
   };
 
   const sortWordCount = (wordCount) => {
-    // Convert word count object to an array of [word, count] pairs
+    // Converting the word count object to an array of [word, count] pairs
     const wordCountArray = Object.entries(wordCount);
 
-    // Sort the array based on the count in descending order
+    // Sorting the array based on the count in descending order(in order to get max frequency)
     wordCountArray.sort((a, b) => b[1] - a[1]);
 
     return wordCountArray;
   };
 
   const handleExport = () => {
-    const csvContent = 'data:text/csv;charset=utf-8,';
-    const csvData = histogramData.map(([word, count]) => `${word},${count}`).join('\n');
-    const encodedURI = encodeURI(csvContent + csvData);
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedURI);
-    link.setAttribute('download', 'histogram.csv');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    const csvSet = 'data:text/csv;charset=utf-8,';
+    const csvInput = histogramData.map(([word, count]) => `${word},${count}`).join('\n');
+    const encodedUrl = encodeURI(csvSet + csvInput);
+    const linkCreate = document.createElement('a');
+    linkCreate.setAttribute('href', encodedUrl);
+    linkCreate.setAttribute('download', 'histogram.csv');
+    document.body.appendChild(linkCreate);
+    linkCreate.click();
+    document.body.removeChild(linkCreate);
   };
 
   return (
@@ -67,8 +67,8 @@ const WordFrequencyAnalyzer = () => {
       </button>
       {histogramData.length > 0 && (
         <>
-          <h2>Word Frequency Histogram</h2>
-          <div className="histogram">
+          <h2>Word Frequency Counter Histogram</h2>
+          <div>
             <Histogram histogramData={histogramData} />
           </div>
           <button onClick={handleExport}>Export</button>
@@ -79,4 +79,4 @@ const WordFrequencyAnalyzer = () => {
   );
 };
 
-export default WordFrequencyAnalyzer;
+export default App;
